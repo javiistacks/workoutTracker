5fe5d91de53f63c4f8171b79a8282d7ace6bf0a7
Javiistacks <javiann.marrero@icloud.com>
2021-05-26 13:35:10 -0500

    added api routes, changed folders and added server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({  extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connected!'))
+  .catch((err) => console.error(err));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
